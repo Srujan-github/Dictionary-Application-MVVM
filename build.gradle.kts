f6d6fd8133b460022033ceeacc0b59105ef48ca5
Srujan-github <thammishettisaisrujan123@gmail.com>
2024-11-03 21:43:46 +0530

    added Detekt and spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,23 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+import com.diffplug.gradle.spotless.SpotlessPlugin
+import io.gitlab.arturbosch.detekt.DetektPlugin
+
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.spotless)
+}
+
+configure(subprojects) {
+    apply<DetektPlugin>()
+    apply<SpotlessPlugin>()
+
+    spotless {
+        kotlin {
+            target("**/*.kt")
+            ktlint("0.43.2")
+        }
+    }
 }
