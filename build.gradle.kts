a7727d808e84076d6e1fba716756139b010739f0
Srujan-github <thammishettisaisrujan123@gmail.com>
2025-01-05 16:27:07 +0530

    Worked on main screen ui

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 import com.diffplug.gradle.spotless.SpotlessPlugin
 import io.gitlab.arturbosch.detekt.DetektPlugin
+import org.jetbrains.kotlin.builtins.StandardNames.FqNames.target
+import org.jetbrains.kotlin.js.translate.context.Namer.kotlin
 
 
 plugins {
     alias(libs.plugins.android.application) apply false
-alias(libs.plugins.jetbrains.kotlin.android) apply false    
-id("com.google.dagger.hilt.android") version "2.48" apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    id("com.google.dagger.hilt.android") version "2.48" apply false
     alias(libs.plugins.detekt)
     alias(libs.plugins.spotless)
 }
@@ -14,11 +16,10 @@ id("com.google.dagger.hilt.android") version "2.48" apply false
 configure(subprojects) {
     apply<DetektPlugin>()
     apply<SpotlessPlugin>()
-
     spotless {
         kotlin {
             target("**/*.kt")
-            ktlint("0.43.2")
+            ktlint("0.48.2")
         }
     }
 }
